88d20f2e6ed0f23e6ed0f25f5c82e18fc3d489e6
Jeff Law <jeffreyalaw@gmail.com>
2021-07-26 10:32:34 -0400

    Update

--- jobs/chroot.sh
+++ jobs/chroot.sh
@@ -13,7 +13,7 @@ patches/jobs/setupsources.sh $TARGET binutils-gdb gcc glibc linux chroots
 # to expose the sources and critically the next job stage script
 # Run the nested container with enough privs to mount filesystems
 # docker run -it 172.31.0.149:5000/gcc-chroot-riscv64-linux-gnu /bin/bash
-docker run -v "$(pwd)"/patches:/home/jlaw/jenkins/workspace/$TARGET/patches --privileged 172.31.0.149:5000/gcc-chroot-$TARGET ls /home/jlaw/jenkins/workspace/$TARGET/patches/jobs
+docker run -v "$(pwd)"/patches:/home/jlaw/jenkins/workspace/$TARGET/patches --privileged 172.31.0.149:5000/gcc-chroot-$TARGET ls -l /home/jlaw/jenkins/workspace/$TARGET/patches
 
 exit 0
 
