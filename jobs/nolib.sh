505ba23eec284e250a7263494166709b12da87bb
Jeff Law <jeffreyalaw@gmail.com>
2021-08-17 00:29:37 -0400

    Use common code to validate test results

--- jobs/nolib.sh
+++ jobs/nolib.sh
@@ -43,25 +43,4 @@ cd ${TARGET}-obj/binutils
 make -k -j $NPROC -l $NPROC check-gas check-ld check-binutils || true
 cd ../..
 
-# Step #4, setup for artifact archiving and comparing testresults
-rm -rf testresults
-mkdir -p testresults
-cp `find ${TARGET}-obj -name \*.sum -print` testresults
-
-newbase=`grep ${TARGET} patches/gcc/NEWBASELINES || true`
-if [ -f old-testresults/gas.sum.gz ]; then
-  rm -f old-testresults/*.sum
-  gunzip old-testresults/*.sum.gz
-  if [ "x$newbase" == "x" ]; then
-    gcc/contrib/compare_tests old-testresults testresults
-  else
-    gcc/contrib/compare_tests old-testresults testresults || true
-  fi
-fi
-
-
-# No need to clean up, everything's run in an ephemeral container
-
-# Step #6, compress testresults before archiving
-cd testresults
-gzip --best *.sum
+patches/jobs/validate-results.sh $TARGET
